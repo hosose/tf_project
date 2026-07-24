e106eb0d2dfd50805689153a0a66b8e1834fa11a
hosose <hosose22@gmail.com>
2026-07-24 15:51:59 +0900

    Feat: 변수 공급자 리전에 표현에 적용

--- basic/provider.tf
+++ basic/provider.tf
@@ -2,14 +2,16 @@ terraform {
   required_version = ">=1.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>6.0"
+      source  = "hashicorp/aws"
+      version = "~>6.0"
     }
   }
 }
 
 provider "aws" {
-    region = "ap-northeast-2"  
+  # region = "ap-northeast-2"  
+  # 변수로 대체
+  region = var.region
 }
 # 공급자 설명, 버전
 # AWS Provider 버전 설명, 서울 리전 지정
