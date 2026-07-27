3f3f10b922516bb70b6f2ae4e226a343faed582a
hosose <hosose22@gmail.com>
2026-07-27 14:24:28 +0900

    feat: add syntax configuration and documentation for terraform step 2

---
+++ tf-step2/syntax.tf
@@ -0,0 +1,44 @@
+variable "name" {
+  # 문자열 타입
+  default = "테라폼"
+}
+variable "age" {
+  # 수치 타입
+  default = 25
+}
+variable "is_mz" {
+  # 불리언 타입
+  default = true
+}
+variable "books" {
+  # 리스트 타입
+  default = ["1", "2"]
+}
+variable "stations" {
+  # 맵 타입
+  default = {
+    A = "신도림",
+    B = "사당",
+  }
+}
+
+# 출력
+output "name" {
+  value = var.name
+}
+
+output "age" {
+  value = var.age
+}
+
+output "is_mz" {
+  value = var.is_mz
+}
+
+output "books" {
+  value = var.books
+}
+
+output "stations" {
+  value = var.stations
+}
