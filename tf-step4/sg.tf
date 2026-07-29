fb0cd2627cad9bd6266d2dc447c7881f4d637d43
hosose <hosose22@gmail.com>
2026-07-29 10:47:25 +0900

    Feat: Rule 대시보드 에서 확인

--- tf-step4/sg.tf
+++ tf-step4/sg.tf
@@ -16,27 +16,100 @@ locals {
       }
     }
     was = {
-      ssh = {
-        port = 22,           # 포트 값 표현
-        cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
+      ingress = {
+        ssh = {
+          port = 22,           # 포트 값 표현
+          cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
+        }
+        # 오직 web -> was 로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
+        #   tcp = {
+        #     port = 8000,         # 포트 값 표현
+        #     cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
+        #   }
       }
-      # 오직 web -> was 로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
-      #   tcp = {
-      #     port = 8000,         # 포트 값 표현
-      #     cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
-      #   }
     }
-  }
-  db = {
-    ssh = {
-      port = 22,           # 포트 값 표현
-      cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
-    }
+    db = {
+      ingress = {
+        ssh = {
+          port = 22,           # 포트 값 표현
+          cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
+        }
 
-    # 오직 was -> db 로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
-    # tcp = {
-    #   port = 3306,         # 포트 값 표현
-    #   cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
-    # }
+        # 오직 was -> db 로만 접근해야함. 취지 벗어남 (다른 방식으로 구성)
+        # tcp = {
+        #   port = 3306,         # 포트 값 표현
+        #   cidr = ["0.0.0.0/0"] # n개 나올 수 있는 허가된 IP값 나열(리스트)
+        # }
+      }
+    }
   }
+}
+
+##############################################
+# Security Groups 생성 선언
+##############################################
+resource "aws_security_group" "sg" {
+  # 반복 데이터로 구성한 locals 주입(세팅) -> web, was,db 총 3개의 멤버를 가짐
+  for_each = local.security_groups
+  # for_each에 Map 타입으로 주입 => each.key, each.value 형태로 반복적, 순서대로 획득
+  # 이름 => DE-AI-07-web-SG, DE-AI-07-was-SG, DE-AI-07-db-SG
+  name_prefix = "DE-AI-07-${each.key}-SG-"
+  description = "${upper(each.key)} Security Group"
+
+  # 보안 그룹은 VPC에 종속되어서 구성됨
+  vpc_id = aws_vpc.DE-AI-07-COMPANY.id
+
+  # ingress 동적 생성 => 블록 반복 구성 => dynamic ingress
+  dynamic "ingress" {
+    # 반복 데이터
+    for_each = each.value.ingress
+    content {
+      protocol    = "tcp"              # 고정값
+      from_port   = ingress.value.port # 80, 22
+      to_port     = ingress.value.port # 80, 22
+      cidr_blocks = ingress.value.cidr # ["0.0.0.0/0"]
+    }
+  }
+
+  # 아웃바운드 고정
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "DE-AI-07-${each.key}-SG"
+  }
+}
+
+##############################################
+# Web -> Was 룰 적용, 포트 8000 오픈, 룰 생성 선언
+##############################################
+resource "aws_security_group_rule" "web-to-was" {
+  type = "ingress"
+  # 소스(web) 보안 그룹
+  source_security_group_id = aws_security_group.sg["web"].id
+  # 타겟(was) 보안 그룹
+  security_group_id = aws_security_group.sg["was"].id
+  # 포트
+  from_port = 8000
+  to_port   = 8000
+  # 프로토콜
+  protocol = "tcp"
+}
+##############################################
+# Was -> Db 룰 적용, 포트 3306 오픈, 룰 생성 선언
+##############################################
+resource "aws_security_group_rule" "web-to-db" {
+  type = "ingress"
+  # 소스(was) 보안 그룹
+  source_security_group_id = aws_security_group.sg["was"].id
+  # 타겟(db) 보안 그룹
+  security_group_id = aws_security_group.sg["db"].id
+  # 포트
+  from_port = 3306
+  to_port   = 3306
+  # 프로토콜
+  protocol = "tcp"
 }
---
+++ tf-step4/vpc.tf
@@ -0,0 +1,52 @@
+# 특정 기업/개인/단체등 전용 VPC 생성 선언
+resource "aws_vpc" "DE-AI-07-COMPANY" {
+  # CIDR 규칙 지정 65536개 IP를 구성할수 있다. 10.0.0.0/16s
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "DE-AI-07-COMPANY"
+  }
+}
+
+# 서브넷 (public)
+resource "aws_subnet" "public" {
+  # 암묵적 의존성 -> 서브넷 구성을 위해서는 반드시 vpc가 먼저 생성되어야함
+  vpc_id = aws_vpc.DE-AI-07-COMPANY.id
+  # CIDR 가용영역 설정, VPC보다 작게
+  cidr_block = "10.0.1.0/24"
+  # 리전마다 가용영역이 a,b,c,d or a,b,c 제한 => 데이터센터 동수
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "DE-AI-07-COMPANY-public-subnet"
+  }
+}
+
+# 인터넷 게이트웨이
+resource "aws_internet_gateway" "company" {
+  # 암묵적 의존성 -> 게이트웨이 구성을 위해서는 반드시 vpc가 먼저 생성되어야함
+  vpc_id = aws_vpc.DE-AI-07-COMPANY.id
+  tags = {
+    Name = "DE-AI-07-COMPANY-igw"
+  }
+}
+
+# 라우트 테이블
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.DE-AI-07-COMPANY.id
+  # 모든 IP 대역 => IGW 전달(연결)
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.company.id
+  }
+  tags = {
+    Name = "DE-AI-07-COMPANY-public-rt"
+  }
+}
+
+# 최종연결 (공개용 서브넷 -> 공개용 라우트테이블)
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
