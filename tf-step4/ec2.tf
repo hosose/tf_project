d14f91238c323eba9654a55ac222e056f2b67d88
hosose <hosose22@gmail.com>
2026-07-29 11:28:23 +0900

    Feat: ec2 반복구성에 대한 설정

--- tf-step4/ec2.tf
+++ tf-step4/ec2.tf
@@ -1,4 +1,20 @@
-# 반복된 내용 locals 구성
+# 반복된 내용 locals 구성 (반복등장/ 값이 상이한것 : 서브넷, 보안그룹)
+locals {
+  servers = {
+    web = {
+      subnet = aws_subnet.public.id
+      sg     = aws_security_group.sg["web"].id
+    }
+    was = {
+      subnet = aws_subnet.private.id
+      sg     = aws_security_group.sg["was"].id
+    }
+    db = {
+      subnet = aws_subnet.private.id
+      sg     = aws_security_group.sg["db"].id
+    }
+  }
+}
 
 # ami 조회
 data "aws_ami" "amazon_linux" {
@@ -19,11 +35,27 @@ data "aws_ami" "amazon_linux" {
   }
 }
 # aws_instance 생성 선언 -> 반복
-
+resource "aws_instance" "server" {
+  for_each      = local.servers
+  instance_type = var.instance_type
+  ami           = data.aws_ami.amazon_linux.id
+  subnet_id     = each.value.subnet
+  key_name      = var.key_name
+  vpc_security_group_ids = [
+    each.value.sg
+  ]
+  tags = {
+    Name = "DE-AI-07-ap2-${upper(each.key)}"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    dnf install -y ec2-instance-connect
+  EOF
+}
 # 오직 web용 EC2만 EIP 생성 선언
 resource "aws_eip" "DE-AI-07-IaC-TF-EIP" {
   # EC2 인스턴스 
-  instance = aws_instance..id
+  #   instance = aws_instance..id
   # 네트워크
   domain = "vpc"
 }
