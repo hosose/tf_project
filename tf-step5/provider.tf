a2f3e86f947b5f05a279a139e9e9c9102ab1ba07
hosose <hosose22@gmail.com>
2026-07-31 11:21:06 +0900

    Feat: provider 구성 완료

---
+++ tf-step5/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+provider "aws" {
+  # region = "ap-northeast-2"  
+  # 변수로 대체
+  region = var.region
+  default_tags {
+    tags = local.common_tags
+  }
+}
+# 공급자 설명, 버전
+# AWS Provider 버전 설명, 서울 리전 지정
+# 변수가 없으므로 하드코딩 했음 => 리전
