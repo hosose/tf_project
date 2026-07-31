b9dd596247c470571850b2f03d02ef8313aeb1d0
hosose <hosose22@gmail.com>
2026-07-31 15:45:49 +0900

    Feat: web 런치 템플릿

---
+++ tf-step5/launch_template.tf
@@ -0,0 +1,35 @@
+###################################
+# web ec2용 -> launch template 요소(ASG 내에서 사용)를 사용하여 생성됨
+###################################
+resource "aws_launch_template" "web" {
+  name_prefix   = "${local.project}-WEB-" # 증감이 수시로 발생해도 중복 x
+  image_id      = data.aws_ami.amazon_linux
+  instance_type = var.instance_type
+  # SSM 접속을 위해 프로파일 설정 -> iam.tf에서 참조
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_ssm.name
+  }
+  # 보안그룹
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  # 서버 구성후 초기 작업 -> 쉘스크립트르 읽어서 -> base64 인코딩처리 -> 실행되게 구성
+  user_data = base64encode(templatefile("${path.module}/userdata-web.sh.tftpl", {
+    internal_alb_dns = aws_lb.internal.dns_name
+  }))
+  # 태그
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(local.common_tags, {
+      Name = "${local.project}-WEB"
+      Tier = "web"
+    })
+  }
+  # 삭제전 생성 -> ec2 수 유지에 대한 기준
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+###################################
+# was ec2용 -> launch template 요소(ASG 내에서 사용)를 사용하여 생성됨
+###################################
