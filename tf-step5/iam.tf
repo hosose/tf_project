739c209102b16dcd228aea2e3a9dfcde2b5a8ae5
hosose <hosose22@gmail.com>
2026-07-31 11:18:19 +0900

    Feat: iam.tf 상세 설명 반영

---
+++ tf-step5/iam.tf
@@ -0,0 +1,33 @@
+#####################################################
+# EC2가 SSM Session Manager를 사용 할 수 있도록 IAM Role 정의 적용
+# -> 서브넷의 유형에 상관없이 접속가능, pen 필요 X
+#####################################################
+# EC2가 IAM 역할을 맡을 수 있도록 허용
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    # 서비스명 ec2 조회 조건 지정
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+# 2. role 생성
+resource "aws_iam_role" "ec2_ssm" {
+  # 프로젝트명을 role에 반영하여 구분할 수 있게 처리
+  # 역할에 policy(정책)
+  name               = "${local.project}-EC2-SSM-Role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+# 3. AmazonSSMManagedInstanceCore(AWS 관리형 정책)에 role 연결
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+# 4. EC2 구성시 적용할 IAM 인스턴스 프로필 생성 -> Launch_template에서 ec2 구성할때 사용
+resource "aws_iam_instance_profile" "ec2_ssm" {
+  name = "${local.project}-EC2-SSM-Profile"
+  role = aws_iam_role.ec2_ssm.name
+}
